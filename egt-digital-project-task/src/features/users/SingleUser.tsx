c934d38ad8ad90445b8f31c8596263bd2e8f2ce6
alexander-hristov91 <alexander.hristov91@gmail.com>
2026-03-06 13:22:11 +0200

    fix: SingleUser full content

--- egt-digital-project-task/src/features/users/SingleUser.tsx
+++ egt-digital-project-task/src/features/users/SingleUser.tsx
@@ -7,13 +7,21 @@ interface SingleUserProps {
 export default function SingleUser({ user }: SingleUserProps) {
   return (
     <>
-    <h2>This is {user.id}</h2>
+    <h1>User{user.id}</h1>
       <h3>{user.name}</h3>
       <h3>{user.username}</h3>
       <h3>{user.email}</h3>
       <h3>{user.address.street}</h3>
       <h3>{user.address.suite}</h3>
       <h3>{user.address.city}</h3>
+      <h3>{user.address.zipcode}</h3>
+      <h3>{user.address.geo.lat}</h3>
+      <h3>{user.address.geo.lng}</h3>
+      <h3>{user.phone}</h3>
+      <h3>{user.website}</h3>
+      <h3>{user.company.name}</h3>
+      <h3>{user.company.catchPhrase}</h3>
+      <h3>{user.company.bs}</h3>
     </>
   );
 }
